396ef827293c35345d2a996c5463814704627d34
Anchit Aggarwal <anchitaggarwal506@gmail.com>
2021-06-06 21:11:46 +0530

    Create Minimum-Cost-to-Move-Chips-to-The-Same-Position.java

---
+++ Minimum-Cost-to-Move-Chips-to-The-Same-Position.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minCostToMoveChips(int[] position) {
+        int even=0;
+        int odd=0;
+        for(int i=0;i<position.length;i++){
+            if(position[i]%2==0) even=even+1;
+            else odd=odd+1;
+        }
+        if(even>odd) return odd;
+        return even;
+    }
+}
